d8d079873d4dec0ee99d56f4690e31942bd8cb5e
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-08-12 09:58:20 +0300

    added bcrypt

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,26 +1,37 @@
 const User = require('../models/clubmodel');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-async function createUser(req,res){
+async function createUser(req,res,next){
     const {username,password,confirmation,email} = req.body;
     try{
-        const user = await User.findOne({email});
-        if (!user){
-            if(password===confirmation){
-                const newUser = new User({
-                    username : username,
-                    email : email,
-                    password : password
-                })
-                await newUser.save()
-                console.log('user created')
-                res.redirect('/') //should go to page for non members
+         // Hash the password using bcrypt
+        bcrypt.hash(password,saltRounds,async(err,hashedPassword)=>{
+            if(err){
+                return next(err);
+            } //00
+            const user = await User.findOne({email});
+            if (!user){
+                if(password===confirmation){
+                    const newUser = new User({
+                        username : username,
+                        email : email,
+                        password : hashedPassword
+                    })
+                    await newUser.save()
+                    console.log('user created')
+                    res.redirect('/') //should go to page for non members
+                }else{
+                    console.log('passwords do not match') //put a message through
+                }
+    
             }else{
-                console.log('passwords do not match') //put a message through
+                console.log('email already in use')//or put  message through
             }
 
-        }else{
-            console.log('email already in use')//or put  message through
-        }
+        })
+
+  
 
     }catch(err){
         console.error(err);
